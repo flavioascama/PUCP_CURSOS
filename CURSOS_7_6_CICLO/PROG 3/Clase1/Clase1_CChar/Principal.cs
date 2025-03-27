2dd832c29faad8bf6c1cd234c10aa96c22d2f64e
flavioascama <a20212426@pucp.edu.pe>
2025-03-27 10:34:03 -0500

    Add files via upload

---
+++ CURSOS_7_6_CICLO/PROG 3/Clase1/Clase1_CChar/Principal.cs
@@ -0,0 +1,7 @@
+class Principal{
+	//PATH="C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe";%PATH%
+	public static void Main(string[] args){
+		System.Console.WriteLine("Hola");
+	}
+
+}
---
+++ CURSOS_7_6_CICLO/PROG 3/Clase1/Clase1_Java/Principal.java
@@ -0,0 +1,14 @@
+class Principal{
+		//Version de uso: Jdk 21
+	//PATH = ruta;%PATH%   //Es temportal hasta que cierre cmd
+	//PROCESO: .java -> compilacion (javac) -> Bytecode (.class) -> Interpretacion (java) -> Resultado en pantalla
+	//se genera un .class por cada clase que tenga en mi fichero
+	//System es una clase
+	//Out es el objeto
+	//println es el metodo
+	
+	///Para c# : se escribe en .cs y se compila en .exe o .ddl
+	public static void main(String[] args){
+			System.out.println("Hi");
+	}
+}
